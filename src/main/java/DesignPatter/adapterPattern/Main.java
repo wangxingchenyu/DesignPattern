cf56724af19c2f23b0db6ba5ccfc899c7ca60b5a
wangxingchenyu <8279179133@qq.com>
2019-12-09 17:48:03 +0800

    designPatterns

---
+++ src/main/java/DesignPatter/adapterPattern/Main.java
@@ -0,0 +1,16 @@
+package main.java.DesignPatter.adapterPattern;
+
+import java.io.FileNotFoundException;
+
+/**
+ * @Author: zhilei.wang
+ * @Date: 2019/12/9 10:24
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(new File("d:/person.data")));
+
+    }
+}
